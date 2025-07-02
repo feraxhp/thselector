ab094c3e3f2d1525f6b43f06ccebb08a94fa5743
feraxhp <feraxhp+gh@gmail.com>
2025-07-02 16:40:55 -0500

    ** rename {sample > docs} - *+ some paddings

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 const set_content = async () => {
-    const content = await fetch("https://raw.githubusercontent.com/feraxhp/grp/refs/heads/main/README.md")
+    const content = await fetch("https://raw.githubusercontent.com/feraxhp/thselector/refs/heads/main/README.md")
     const text = await content.text();
     
     document.getElementById('content').innerHTML = marked.parse(text);
